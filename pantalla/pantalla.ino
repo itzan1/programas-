c0615c37a0b892d811be873661235e87a9f629d4
itzan1 <juancho_339@hotmail.com>
2018-06-08 16:32:38 -0600

    modificacion en laboratorio
    se ha modificado el codigo y agregado la segunda pantalla de SET VOLTAJE

--- pantalla/pantalla.ino
+++ pantalla/pantalla.ino
@@ -21,10 +21,10 @@ extern uint8_t BigFont[];
 extern uint8_t SevenSegNumFont[];
 
 //variables que estaremos utilizando
-int x,y;
+int x,y,pantalla=1,k,vout=0,a=0;
 
 
-void botones(){
+void botones1(){ //pantalla 1
   myGLCD.setFont(BigFont); 
   for (x=0; x<3; x++)
   {
@@ -59,8 +59,9 @@ for (x=0; x<2; x++)
   
   }
 
-
-void marco(int x1, int y1, int x2, int y2){
+ 
+  
+void marco1(int x1, int y1, int x2, int y2){ //marcos pantalla 1
   myGLCD.setColor(255, 0, 0);
   myGLCD.drawRoundRect (x1, y1, x2, y2);
   while (myTouch.dataAvailable())
@@ -70,20 +71,7 @@ void marco(int x1, int y1, int x2, int y2){
   
 }
 
-
-
-void setup(){
-  myGLCD.InitLCD();
-  myGLCD.clrScr();
-  myTouch.InitTouch();
-  myTouch.setPrecision(PREC_HI);
-  botones();
-  }
-
-
-
-
-void touch(){
+void touch1(){
   
       myTouch.read();
       x=myTouch.getX();
@@ -92,18 +80,17 @@ void touch(){
       if((x>=200) && (x<=310))
       {
         if((y>=10) && (y<=60)){ //boton ON 2
-          marco(200,10,310,60);
-
+          marco1(200,10,310,60);
         }
         
         if((y>=65) && (y<=115)){ //boton ON 3
-          marco(200,65,310,115);
+          marco1(200,65,310,115);
 
         }
 
         if((y>=120) && (y<=170)){ //boton ON 1
-          marco(200,120,310,170);
-
+          marco1(200,120,310,170);
+   
         }
 
        
@@ -112,25 +99,163 @@ void touch(){
       if((y>=175) && (y<=225))
       {
         if((x>=10) && (x<=155)){ //boton V SET
-        
-          marco(10,175,155,225);
-
+          marco1(10,175,155,225);
+          pantalla =2;
         }
-      }
-        if((x>=165) && (x<=310)){ //boton V SET
-        
-          marco(165,175,310,225);
-
+      
+        if((x>=165) && (x<=310)){ //boton config
+          marco1(165,175,310,225);
+          
         }
 }
+}
 
-        
+////////////////////////////// fin de pantalla 1
+
+
+
+////////////////////////////// pantalla 2
+
+void botones2(){ //pantalla 2
+  
+  for (x=0; x<4; x++) //botones +
+  {
+    myGLCD.setColor(0, 0, 255);
+    myGLCD.fillRoundRect (10+(x*60), 10, 60+(x*60), 60);
+    myGLCD.setColor(255, 255, 255);
+    myGLCD.drawRoundRect (10+(x*60), 10, 60+(x*60), 60);
+    myGLCD.print("+", 27+(x*60), 27);
+  }
+
+
+  for (x=0; x<4; x++) //botones -
+  {
+    myGLCD.setColor(0, 0, 255);
+    myGLCD.fillRoundRect (10+(x*60), 170, 60+(x*60), 220);
+    myGLCD.setColor(255, 255, 255);
+    myGLCD.drawRoundRect (10+(x*60), 170, 60+(x*60), 220);
+    myGLCD.print("-", 27+(x*60), 190);
+  }
+
+  for (x=0; x<4; x++) //blanco 
+  {
+    myGLCD.setColor(255, 255, 255);
+    myGLCD.fillRoundRect (10+(x*60), 70, 60+(x*60), 160);
+    myGLCD.setColor(255, 0, 0);
+    myGLCD.drawRoundRect (10+(x*60), 70, 60+(x*60), 160);
+    //myGLCD.print("-", 27+(x*60), 170);
+  }
+
+  myGLCD.setColor(0, 0, 255); /// boton set
+   myGLCD.fillRoundRect(250,70,310,160);
+   myGLCD.setColor(255, 255, 255);
+   myGLCD.drawRoundRect(250,70,310,160);
+   myGLCD.print("set" , 255,105);
+  }
+  
+   void touch2(){
+    myTouch.read();
+      x=myTouch.getX();
+      y=myTouch.getY();
+
+      if((y>=10) && (y<=60)){ /////////////botones +
+        if((x>=10) && (x<=60)){ //boton + kilos
+          marco1(10,10,60,60);
+//          myGLCD.setFont(SevenSegNumFont); 
+//          vout=vout+1000;
+//          a=a+1;
+//          myGLCD.print(string.toInt(a),20,70);
+          
+        }
+
+        if((x>=70) && (x<=120)){ //boton + kilos
+          marco1(70,10,120,60);
+        }
+
+        if((x>=130) && (x<=180)){ //boton + kilos
+          marco1(130,10,180,60);
+        }
+
+        if((x>=190) && (x<=240)){ //boton + kilos
+          marco1(190,10,240,60);
+        }
+        }
+
+
+        if((y>=170) && (y<=220)){ ////////////botones -
+          
+        if((x>=10) && (x<=60)){ //boton + kilos
+          marco1(10,170,60,220);
+        }
+
+        if((x>=70) && (x<=120)){ //boton + kilos
+          marco1(70,170,120,220);
+        }
+
+        if((x>=130) && (x<=180)){ //boton + kilos
+          marco1(130,170,180,220);
+        }
+
+        if((x>=190) && (x<=240)){ //boton + kilos
+          marco1(190,170,240,220);
+        }
+        }
+
+        if((x>=250) && (x<=310)){ // boton SET
+
+          if((y>=70) && (y<=160)){
+            marco1(250,70,310,160);
+            pantalla =1;
+            
+            }
+          
+
+        }
+       
     
+    }
 
-  void loop(){
+
+///////////////////////fin pantalla set voltaje
+
+void setup(){
+  myGLCD.InitLCD();
+  myGLCD.clrScr();
+  myTouch.InitTouch();
+  myTouch.setPrecision(PREC_HI);
+
+  }
+
+void loop(){
+
+//pantalla 1
+if(pantalla == 1){
+    myGLCD.fillScr(VGA_BLACK);
+    botones1();
+while(true){
 if(myTouch.dataAvailable()){
 
-  touch();
+  touch1();
   }
+
+  if(pantalla == 2 )break;
+  }
+}
+
+if(pantalla == 2){
+  myGLCD.fillScr(VGA_BLACK);
+  botones2();
+  while(true)
+    {
+      if(myTouch.dataAvailable()){
+
+  touch2();
+  }
+
+  if(pantalla == 1){break;}
+    }
+  }
+
+
   }
 
