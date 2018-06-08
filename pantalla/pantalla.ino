98c5fefa321393bd7ae2f4addbcabe767abdbb18
itzan1 <juancho_339@hotmail.com>
2018-06-08 13:00:21 -0600

    Pantalla 2
    Se ha ordenado el codigo, agregado los botones que se necesitan y las funciones touch.  Falta pantalla para VSET, CONFIG y las funciones de botones ON

--- pantalla/pantalla.ino
+++ pantalla/pantalla.ino
@@ -1,116 +1,136 @@
-#include <UTFT.h> 
+/////////////////////////////////////
+//programa para control de alto voltaje
+//mediante la implementacion de LCD TOUCH TFT de 3.2´´
+//
+//
+//
+/////////////////////////////////////
+#include <UTFT.h>
 #include <URTouch.h>
-int a=1;
-//indicamos pines para hardware
 
+
+
+//indicamos pines para hardware
 UTFT    myGLCD(ILI9341_16,38,39,40,41);
 URTouch  myTouch( 6, 5, 4, 3, 2);
 
-//==== Definimos fuentes que utilizaremos
+
+//Definimos fuentes que utilizaremos
 extern uint8_t SmallFont[];
 extern uint8_t BigFont[];
 extern uint8_t SevenSegNumFont[];
 
-void setup() {
-  // Inicializamos pantalla y touch
+//variables que estaremos utilizando
+int x,y;
+
+
+void botones(){
+  myGLCD.setFont(BigFont); 
+  for (x=0; x<3; x++)
+  {
+    myGLCD.setColor(0, 0, 255);
+    myGLCD.fillRoundRect (200, 10+(x*55), 310, 60+(x*55));
+    myGLCD.setColor(255, 255, 255);
+    myGLCD.drawRoundRect (200, 10+(x*55), 310, 60+(x*55));
+    
+  }
+
+for (x=0; x<2; x++)
+  {
+    myGLCD.setColor(0, 0, 255);
+    myGLCD.fillRoundRect (10+(x*155), 175, 155+(x*155), 225);
+    myGLCD.setColor(255, 255, 255);
+    myGLCD.drawRoundRect (10+(x*155), 175, 155+(x*155), 225);
+    
+  }
+
+
+
+  
+  myGLCD.setBackColor(0, 0, 255);
+  myGLCD.print("ON 2", 220 , 30);
+  myGLCD.print("ON 3", 220 , 85);
+  myGLCD.print("ON 1", 220 , 140);
+  //myGLCD.print("UART ON", 185 , 195);
+  myGLCD.print("V SET", 40 , 190);
+  myGLCD.print("CONFIG", 190 , 190);
+  
+  
+  
+  }
+
+
+void marco(int x1, int y1, int x2, int y2){
+  myGLCD.setColor(255, 0, 0);
+  myGLCD.drawRoundRect (x1, y1, x2, y2);
+  while (myTouch.dataAvailable())
+  myTouch.read();
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.drawRoundRect (x1, y1, x2, y2);
+  
+}
+
+
+
+void setup(){
   myGLCD.InitLCD();
   myGLCD.clrScr();
   myTouch.InitTouch();
-  myTouch.setPrecision(PREC_MEDIUM);
-int a=1;
+  myTouch.setPrecision(PREC_HI);
+  botones();
+  }
+
+
+
+
+void touch(){
+  
+      myTouch.read();
+      x=myTouch.getX();
+      y=myTouch.getY();
+
+      if((x>=200) && (x<=310))
+      {
+        if((y>=10) && (y<=60)){ //boton ON 2
+          marco(200,10,310,60);
+
+        }
+        
+        if((y>=65) && (y<=115)){ //boton ON 3
+          marco(200,65,310,115);
+
+        }
+
+        if((y>=120) && (y<=170)){ //boton ON 1
+          marco(200,120,310,170);
+
+        }
+
+       
+      }
+
+      if((y>=175) && (y<=225))
+      {
+        if((x>=10) && (x<=155)){ //boton V SET
+        
+          marco(10,175,155,225);
+
+        }
+      }
+        if((x>=165) && (x<=310)){ //boton V SET
+        
+          marco(165,175,310,225);
+
+        }
 }
-void principal(){
-  
-// myGLCD.setBackColor(0,0,0); // Sets the background color of the area where the text will be printed to black
-  myGLCD.setColor(255, 255, 255); // Sets color to white
-  myGLCD.setFont(BigFont); // Sets font to big
-  myGLCD.print("Modo Normal 0V-999V", CENTER, 0); // Prints the string on the screen
 
-  //BOTON SET VOLTAJE
-
-myGLCD.setColor(16, 167, 103); // Sets green color
-  myGLCD.fillRoundRect (180, 40, 285, 70); // Draws filled rounded rectangle
-  myGLCD.setColor(255, 255, 255); // Sets color to white
-  myGLCD.drawRoundRect (180, 40, 285, 70); // Draws rounded rectangle without a fill, so the overall appearance of the button looks like it has a frame
-  myGLCD.setFont(SmallFont); // Sets the font to big
-  myGLCD.setBackColor(16, 167, 103); // Sets the background color of the area where the text will be printed to green, same as the button
-  myGLCD.print("SET VOLTAJE", 190, 50); // Prints the string
-
-  //MODO DE FUNCIONAMIENTO
-
-myGLCD.setColor(16, 167, 103); // Sets green color
-  myGLCD.fillRoundRect (180, 80, 285, 120); // Draws filled rounded rectangle
-  myGLCD.setColor(255, 255, 255); // Sets color to white
-  myGLCD.drawRoundRect (180, 80, 285, 120); // Draws rounded rectangle without a fill, so the overall appearance of the button looks like it has a frame
-  myGLCD.setFont(SmallFont); // Sets the font to big
-  myGLCD.setBackColor(16, 167, 103); // Sets the background color of the area where the text will be printed to green, same as the button
-  myGLCD.print("CONFIGURAR", 190, 95); // Prints the string
-
-
-  
-  
-//IMPRIMIR LECTURA DE VOLTAJE
-  myGLCD.setBackColor(0, 0, 0); 
-  myGLCD.setFont(SevenSegNumFont); 
-  myGLCD.setColor(255, 0, 0);
-  myGLCD.print("800", 20, 30); 
-  myGLCD.setFont(BigFont); 
-  myGLCD.setColor(255, 255, 255);
-  myGLCD.print("V", 130, 60); 
-
- //IMPRIMIR VOLTAJE SET
- myGLCD.setBackColor(0, 0, 0); 
-  myGLCD.setFont(SmallFont); 
-  myGLCD.setColor(255, 255, 255);
-  myGLCD.print("800", 30, 85); 
-  myGLCD.setFont(SmallFont); 
-  myGLCD.setColor(255, 255, 255);
-  myGLCD.print("V SET", 70, 85); 
-  //IMPRIMIR CORRIENTE
-  
-  myGLCD.setFont(BigFont); 
-  myGLCD.setColor(255, 0, 0);
-  myGLCD.print("5.8", 70, 100); // Prints the string on the screen
-  
-  myGLCD.setFont(BigFont); 
-  myGLCD.setColor(255, 255, 255);
-  myGLCD.print("mA", 130, 100); // Prints the string on the screen
-
-    //IMPRIMIR potencia
-  
-  myGLCD.setFont(BigFont); 
-  myGLCD.setColor(255, 0, 0);
-  myGLCD.print("180", 70, 130); // Prints the string on the screen
-  
-  myGLCD.setFont(BigFont); 
-  myGLCD.setColor(255, 255, 255);
-  myGLCD.print("W", 130, 130); // Prints the string on the screen
-
-  //ALIMENTACION
-
-  myGLCD.setFont(SmallFont); 
-  myGLCD.setColor(255, 255, 255);
-  myGLCD.print("INPUT: ", 10, 225);
-  
-  myGLCD.setFont(SmallFont); 
-  myGLCD.setColor(255, 0, 0);
-  myGLCD.print("12V", 60, 225);
-
-  //SEND DATA
-
-  myGLCD.setFont(SmallFont); 
-  myGLCD.setColor(255, 255, 255);
-  myGLCD.print("ENVIANDO DATOS ", 205, 225);
-
-  
-  
-  }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  if(a==1){
+        
     
-    principal();
-    a=0;
+
+  void loop(){
+if(myTouch.dataAvailable()){
+
+  touch();
   }
   }
+
